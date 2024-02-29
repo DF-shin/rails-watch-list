57a59d3fa2660683fa48c3dd34926b488a49cf52
declan schindler <declanschindler0929@live.com>
2024-02-29 17:12:17 -0500

    Back-end is nearly complete, cleaning up css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
-  resources :lists, only: %w[create new show destroy]
+  resources :lists, only: %w[create new show destroy] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
